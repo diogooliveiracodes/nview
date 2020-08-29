84510b3977172d003e993bcd5deba6a79f908fd4
Diogo Oliveira <diogo.o@icloud.com>
2020-08-29 11:31:48 -0300

    Arquivos JS/CSS

--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\laravel\Escola\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\laravel\Escola\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/diogo/nview/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/diogo/nview/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
