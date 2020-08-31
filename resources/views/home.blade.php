0735c2fe4f934c640ee669658bb6c8034ce335c6
Diogo Oliveira <diogo.o@icloud.com>
2020-08-31 20:56:09 -0300

    dashboard inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
-<div class="container">
+
+<div class="container bg-dark">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
