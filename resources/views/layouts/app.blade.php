b9dc4b30b753e5d2f7456596a6a0faf382ee9537
Diogo Oliveira <diogo.o@icloud.com>
2020-08-31 22:13:40 -0300

    edição inicial navbar logada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,18 +55,21 @@
                             @endif
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" style="color:white;" href="{{ route('home') }}">{{ __('Dashboard') }}</a>
+                                <a class="" style="color:white; font-size:1rem" href="{{ route('home') }}">
+                                    <button class="btn btn-outline-light"><strong>Painel Administrativo</strong></button>
+                                </a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+
+                            <li class="nav-item dropdown ml-2">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Sair <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Confirmar Saída') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
